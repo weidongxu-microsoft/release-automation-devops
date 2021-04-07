51a83bf884e2bffad2d30333c60d53559a99e2a3
Weidong Xu <weidxu@microsoft.com>
2021-04-07 13:33:27 +0800

    approve pr and merge

--- src/main/java/com/azure/dev/main/LiteMain.java
+++ src/main/java/com/azure/dev/main/LiteMain.java
@@ -1,17 +1,109 @@
 package com.azure.dev.main;
 
+import com.azure.core.credential.BasicAuthenticationCredential;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
 import com.azure.core.util.Configuration;
+import com.azure.dev.DevManager;
+import com.azure.dev.models.Run;
+import com.azure.dev.models.RunPipelineParameters;
+import com.azure.dev.models.RunState;
+import com.azure.dev.models.Variable;
+import com.spotify.github.v3.clients.GitHubClient;
+import com.spotify.github.v3.clients.PullRequestClient;
+import com.spotify.github.v3.clients.RepositoryClient;
+import com.spotify.github.v3.prs.ImmutableMergeParameters;
+import com.spotify.github.v3.prs.ImmutableReviewParameters;
+import com.spotify.github.v3.prs.MergeMethod;
+import com.spotify.github.v3.prs.PullRequest;
+import com.spotify.github.v3.prs.PullRequestItem;
+import com.spotify.github.v3.prs.Review;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 public class LiteMain {
 
-    private static final String USER = Configuration.getGlobalConfiguration().get("USER");
-    private static final String PASS = Configuration.getGlobalConfiguration().get("PAT");
+    private static final String USER = Configuration.getGlobalConfiguration().get("DEVOPS_USER");
+    private static final String PASS = Configuration.getGlobalConfiguration().get("DEVOPS_PAT");
     private static final String ORGANIZATION = "azure-sdk";
     private static final String PROJECT = "internal";
 
-    private static final int BUILD_ID = Integer.parseInt(Configuration.getGlobalConfiguration().get("BUILD_ID"));
+    private static final String GITHUB_TOKEN = Configuration.getGlobalConfiguration().get("GITHUB_PAT");
+    private static final String GITHUB_ORGANIZATION = "Azure";
+    private static final String GITHUB_PROJECT = "azure-sdk-for-java";
 
-    public static void main(String[] args) throws InterruptedException {
+    private static final int LITE_CODEGEN_PIPELINE_ID = 2238;
 
+    public static void main(String[] args) throws Exception {
+        TokenCredential tokenCredential = new BasicAuthenticationCredential(USER, PASS);
+
+        DevManager manager = DevManager.configure()
+                .withLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+                .withPolicy(new BasicAuthAuthenticationPolicy(tokenCredential))
+                .authenticate(
+                        new BasicAuthenticationCredential(USER, PASS),
+                        new AzureProfile(AzureEnvironment.AZURE));
+
+        GitHubClient github = GitHubClient.create(new URI("https://api.github.com/"), GITHUB_TOKEN);
+        RepositoryClient client = github.createRepositoryClient(GITHUB_ORGANIZATION, GITHUB_PROJECT);
+
+        String sdk = "cost-management";
+
+        Map<String, Variable> variables = new HashMap<>();
+        variables.put("README", new Variable().withValue(sdk));
+        variables.put("TAG", new Variable().withValue("package-2019-11"));
+
+        //runLiteCodegen(manager, variables);
+
+        mergeGithubPR(client, sdk);
+    }
+
+    private static void runLiteCodegen(DevManager manager, Map<String, Variable> variables) throws InterruptedException {
+        Run run = manager.runs().runPipeline(ORGANIZATION, PROJECT, LITE_CODEGEN_PIPELINE_ID,
+                new RunPipelineParameters().withVariables(variables));
+        int runId = run.id();
+
+        // wait for complete
+        while (run.state() != RunState.COMPLETED && run.state() != RunState.CANCELING) {
+            System.out.println("run id " + runId + ", state " + run.state());
+
+            System.out.println("wait 1 minutes");
+            Thread.sleep(60 * 1000);
+
+            run = manager.runs().get(ORGANIZATION, PROJECT, LITE_CODEGEN_PIPELINE_ID, runId);
+        }
+    }
+
+    private static void mergeGithubPR(RepositoryClient client, String sdk) throws ExecutionException, InterruptedException {
+        PullRequestClient prClient = client.createPullRequestClient();
+        List<PullRequestItem> prs = prClient.list().get();
+
+        PullRequestItem prItem = prs.stream()
+                .filter(p -> p.title().startsWith("[Automation]") && p.title().contains(sdk))
+                .findFirst().orElse(null);
+
+        if (prItem != null) {
+            int prNumber = prItem.number();
+            Review review = prClient.createReview(prItem.number(),
+                    ImmutableReviewParameters.builder().event("APPROVE").build()).get();
+
+            PullRequest pr = prClient.get(prNumber).get();
+            while (!pr.mergeable().get()) {
+                System.out.println("pr number " + prNumber + ", mergeable " + pr.mergeable());
+
+                System.out.println("wait 1 minutes");
+                Thread.sleep(60 * 1000);
+            }
+
+            prClient.merge(prNumber,
+                    ImmutableMergeParameters.builder().sha(pr.head().sha()).mergeMethod(MergeMethod.squash).build()).get();
+        }
     }
 }
