d46f4cf639746f63c6cfba8b976e7d7c503d8bc6
Weidong Xu <weidxu@microsoft.com>
2024-01-24 13:51:10 +0800

    create as draft pr

--- src/main/java/io/weidongxu/util/releaseautomation/Utils.java
+++ src/main/java/io/weidongxu/util/releaseautomation/Utils.java
@@ -1,9 +1,14 @@
 package io.weidongxu.util.releaseautomation;
 
+import com.azure.core.exception.HttpResponseException;
+import com.azure.core.http.HttpHeaderName;
 import com.azure.core.http.HttpMethod;
 import com.azure.core.http.HttpPipeline;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.HttpResponse;
+import com.azure.core.util.serializer.JacksonAdapter;
+import com.azure.core.util.serializer.SerializerAdapter;
+import com.azure.core.util.serializer.SerializerEncoding;
 import com.azure.dev.DevManager;
 import com.azure.dev.models.Timeline;
 import com.azure.dev.models.TimelineRecord;
@@ -14,10 +19,12 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.UncheckedIOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -25,6 +32,7 @@ import java.util.stream.Collectors;
 public class Utils {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
+    private static final SerializerAdapter SERIALIZER_ADAPTER = JacksonAdapter.createDefaultSerializerAdapter();
 
     public static ReleaseState getReleaseState(TimelineRecord record, Timeline timeline) {
         ReleaseState state = new ReleaseState();
@@ -59,7 +67,7 @@ public class Utils {
                     HttpMethod.PATCH,
                     String.format("https://dev.azure.com/%s/%s/_apis/pipelines/approvals/%s?api-version=6.0-preview", organization, project, approvalId.toString()));
             request.setBody(String.format("[{\"approvalId\": \"%s\", \"status\": 4, \"comment\": \"\"}]", approvalId));
-            request.setHeader("content-type", "application/json");
+            request.setHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
             HttpResponse response = manager.serviceClient().getHttpPipeline().send(request).block();
             System.out.println("response status code: " + response.getStatusCode());
             if (response.getStatusCode() != 200) {
@@ -70,6 +78,108 @@ public class Utils {
             } else {
                 response.close();
             }
+        }
+    }
+
+
+    public static CheckRunListResult getCheckRuns(HttpPipeline httpPipeline, String token, String sha) {
+        HttpRequest request = new HttpRequest(HttpMethod.GET,
+                "https://api.github.com/repos/Azure/azure-sdk-for-java/commits/" + sha + "/check-runs");
+        request.setHeader(HttpHeaderName.AUTHORIZATION, "token " + token)
+                .setHeader(HttpHeaderName.ACCEPT, "application/json")
+                .setHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
+
+        HttpResponse response = httpPipeline.send(request).block();
+
+        if (response.getStatusCode() == 200) {
+            String body = response.getBodyAsString().block();
+            try {
+                return SERIALIZER_ADAPTER.deserialize(body, CheckRunListResult.class, SerializerEncoding.JSON);
+            } catch (IOException e) {
+                LOGGER.error("error in response body {}", body);
+                throw new HttpResponseException(response);
+            } finally {
+                response.close();
+            }
+        } else {
+            LOGGER.error("error in response code {}, body {}", response.getStatusCode(), response.getBodyAsString().block());
+            response.close();
+            throw new HttpResponseException(response);
+        }
+    }
+
+    public static CommitStatus[] getCommitStatuses(HttpPipeline httpPipeline, String token, String sha) {
+        CommitStatus[] statuses = {};
+
+        HttpRequest request = new HttpRequest(HttpMethod.GET,
+                "https://api.github.com/repos/Azure/azure-sdk-for-java/statuses/" + sha);
+        request.setHeader(HttpHeaderName.AUTHORIZATION, "token " + token)
+                .setHeader(HttpHeaderName.ACCEPT, "application/json")
+                .setHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
+
+        HttpResponse response = httpPipeline.send(request).block();
+
+        if (response.getStatusCode() == 200) {
+            String body = response.getBodyAsString().block();
+            try {
+                statuses = SERIALIZER_ADAPTER.deserialize(body, statuses.getClass(), SerializerEncoding.JSON);
+                return statuses;
+            } catch (IOException e) {
+                LOGGER.error("error in response body {}", body);
+                throw new HttpResponseException(response);
+            } finally {
+                response.close();
+            }
+        } else {
+            LOGGER.error("error in response code {}, body {}", response.getStatusCode(), response.getBodyAsString().block());
+            response.close();
+            throw new HttpResponseException(response);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void prReady(HttpPipeline httpPipeline, String token, int prNumber) {
+        String query = "{repository(owner: \"Azure\", name: \"azure-sdk-for-java\") {pullRequest(number: " + prNumber + ") {id}}}";
+
+        HttpRequest request = new HttpRequest(HttpMethod.POST,
+                "https://api.github.com/graphql");
+        request.setBody("{\"query\": \"" + query.replace("\"", "\\\"") + "\"}");
+        request.setHeader(HttpHeaderName.AUTHORIZATION, "bearer " + token)
+                .setHeader(HttpHeaderName.ACCEPT, "application/json")
+                .setHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
+
+        HttpResponse response = httpPipeline.send(request).block();
+
+        String id = null;
+        if (response.getStatusCode() == 200) {
+            String body = response.getBodyAsString().block();
+            try {
+                Map<String, Object> json = SERIALIZER_ADAPTER.deserialize(body, Map.class, SerializerEncoding.JSON);
+                id = ((Map<String, String>) ((Map<String, Object>) ((Map<String, Object>) json.get("data")).get("repository")).get("pullRequest")).get("id");
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        } else {
+            LOGGER.error("error in response code {}, body {}", response.getStatusCode(), response.getBodyAsString().block());
+            response.close();
+            throw new HttpResponseException(response);
+        }
+
+        String mutation = "mutation {markPullRequestReadyForReview(input: {pullRequestId: \"" + id + "\"}) {pullRequest{id,number,isDraft}}}";
+
+        request = new HttpRequest(HttpMethod.POST,
+                "https://api.github.com/graphql");
+        request.setBody("{\"query\": \"" + mutation.replace("\"", "\\\"") + "\"}");
+        request.setHeader(HttpHeaderName.AUTHORIZATION, "bearer " + token)
+                .setHeader(HttpHeaderName.ACCEPT, "application/json")
+                .setHeader(HttpHeaderName.CONTENT_TYPE, "application/json");
+
+        response = httpPipeline.send(request).block();
+
+        if (response.getStatusCode() != 200) {
+            LOGGER.error("error in response code {}, body {}", response.getStatusCode(), response.getBodyAsString().block());
+            response.close();
+            throw new HttpResponseException(response);
         }
     }
 
